231180556c073d157a8f51d103adf315f1096be9
Musi <musi@pm.nttdata.co.jp>
2018-04-10 16:47:22 +0900

    git-plus

--- template/pages/login/index.vue
+++ template/pages/login/index.vue
@@ -1,16 +1,77 @@
 <template>
 <section>
-  <h1>THE LOGIN PAGE</h1>
-  <div class="view">
-    <form role="form">
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email"></div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></div>
-      <button type="submit" class="btn btn-default">Submit</button>
-    </form>
+  <div class="overlay"></div>
+  <div class="gtco-container">
+    <div class="row">
+      <div class="col-md-12 col-md-offset-0 text-left">
+
+
+        <div class="row row-mt-15em">
+          <div class="col-md-7 mt-text animate-box" data-animate-effect="fadeInUp">
+            <h1>Planing Trip To Anywhere in The World?</h1>
+          </div>
+          <div class="col-md-4 col-md-push-1 animate-box" data-animate-effect="fadeInRight">
+            <div class="form-wrap">
+              <div class="tab">
+
+                <div class="tab-content">
+                  <div class="tab-content-inner active" data-content="signup">
+                    <h3>Book Your Trip</h3>
+                    <form action="#">
+                      <div class="row form-group">
+                        <div class="col-md-12">
+                          <label for="fullname">Your Name</label>
+                          <input type="text" id="fullname" class="form-control">
+                        </div>
+                      </div>
+                      <div class="row form-group">
+                        <div class="col-md-12">
+                          <label for="activities">Activities</label>
+                          <select name="#" id="activities" class="form-control">
+                          <option value="">Activities</option>
+                          <option value="">Hiking</option>
+                          <option value="">Caving</option>
+                          <option value="">Swimming</option>
+                        </select>
+                        </div>
+                      </div>
+                      <div class="row form-group">
+                        <div class="col-md-12">
+                          <label for="destination">Destination</label>
+                          <select name="#" id="destination" class="form-control">
+                          <option value="">Philippines</option>
+                          <option value="">USA</option>
+                          <option value="">Australia</option>
+                          <option value="">Singapore</option>
+                        </select>
+                        </div>
+                      </div>
+
+                      <div class="row form-group">
+                        <div class="col-md-12">
+                          <label for="date-start">Date Travel</label>
+                          <input type="text" id="date-start" class="form-control">
+                        </div>
+                      </div>
+
+                      <div class="row form-group">
+                        <div class="col-md-12">
+                          <input type="submit" class="btn btn-primary btn-block" value="Submit">
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
   </div>
 </section>
 </template>
